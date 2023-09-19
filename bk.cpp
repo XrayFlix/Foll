8193a7adefaedbdfd8982a56d88328445eb1e903
XrayFlix <145455567+XrayFlix@users.noreply.github.com>
2023-09-19 14:54:41 +0300

    Калькулятор

---
+++ bk.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a = 0;
+    int b = 0;
+    char op;
+    cout << "Число 1: ";
+    cin >> a;
+    cout << "Число 2: ";
+    cin >> b;
+    cout << "Операция: ";
+    cin >> op;
+    if (op == '+') {
+        cout << a << "+" << b << "=" << a + b;
+    }
+    else if (op == '-') {
+        cout << a << "-" << b << "=" << a - b;
+    }
+    else if (op == '*') {
+        cout << a << "*" << b << "=" << a * b;
+    }
+    else if (op == '/') {
+        cout << a << "/" << b << "=" << a / b;
+    }
+    
+
+}
+
+
